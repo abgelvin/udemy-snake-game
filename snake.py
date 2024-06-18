a490cc927c3e27f099d7e4f18f2c31571eb00116
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-06-18 17:30:41 -0600

    created food class

--- snake.py
+++ snake.py
@@ -8,12 +8,13 @@ LEFT = 180
 RIGHT = 0
 
 
-class Snake():
+class Snake:
     def __init__(self):
         # self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -23,6 +24,7 @@ class Snake():
             new_segment.goto(position)
             self.segments.append(new_segment)
 
+
     def move(self):
         for seg in range(len(self.segments) -1, 0, -1):
             new_x = self.segments[seg - 1].xcor()
@@ -30,21 +32,22 @@ class Snake():
             self.segments[seg].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+
     def up(self):
-        if self.heading() != DOWN:
+        if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
 
     def down(self):
-        if self.heading() != UP:
+        if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
 
     def left(self):
-        if self.heading() != RIGHT:
+        if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
 
     def right(self):
-        if self.heading() != LEFT:
-            self.head.setheading(RIGHT)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT) 
