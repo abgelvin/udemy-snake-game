3669e9a9f01fe1c86fa531164e6645196ee9593b
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-06-26 15:45:26 -0600

    save high score to txt file

--- main.py
+++ main.py
@@ -35,15 +35,17 @@ while game_is_on:
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        # game_is_on = False
+        scoreboard.reset()
+        snake.reset()
+        # scoreboard.game_over()
 
 
     #Detect collisoin with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            # game_is_on = False
+            scoreboard.reset()
     
 
 
--- snake.py
+++ snake.py
@@ -28,13 +28,20 @@ class Snake:
         self.segments.append(new_segment)
 
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
 
-
     def move(self):
-        for seg in range(len(self.segments) -1, 0, -1):
+        for seg in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg - 1].xcor()
             new_y = self.segments[seg - 1].ycor()
             self.segments[seg].goto(new_x, new_y)
